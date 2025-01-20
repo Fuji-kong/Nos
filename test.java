998bd1a6151a63eb1d5f5331071eca37f73c70f2
Fuji-kong <Kryantjinwangdali1@gmail.com>
2025-01-20 11:53:42 +0800

    RAWWWW

---
+++ test.java
@@ -0,0 +1,18 @@
+
+
+import java.util.Scanner;
+
+public class test { 
+
+public static void main (String [] args) {
+
+Scanner scn = new Scanner (System.in);
+System.out.println("Joe Biden 69 password");
+int joe = scn.nextInt();
+
+if (joe == 69) {System.out.println("WOW YOU GOT IT RIGHT");}
+else System.out.println("WRONNGGGG SULFURIC ACID");
+
+scn.close();
+} 
+}
