5652e3c9265c1bd0c6de0267b1196dedf190998d
Fuji-kong <Kryantjinwangdali1@gmail.com>
2025-01-20 14:25:17 +0800

    thsis from vscode

--- test.java
+++ test.java
@@ -7,7 +7,7 @@ public class test {
 public static void main (String [] args) {
 
 Scanner scn = new Scanner (System.in);
-System.out.println("Joe Biden 69 password");
+System.out.println("Joe Biden 69 password:");
 int joe = scn.nextInt();
 
 if (joe == 69) {System.out.println("WOW YOU GOT IT RIGHT");}
